eb690eb72b057867b2bf112efbc667b3ace59be4
Yang Xing <yangxingdy@163.com>
2012-12-15 00:41:28 +0800

    Add ParkingManager & RandomParkingLotChooser

--- src/ParkingManager.java
+++ src/ParkingManager.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Yang Xing
@@ -5,5 +7,10 @@
  * Time: 下午11:19
  * To change this template use File | Settings | File Templates.
  */
-public class ParkingManager {
+public class ParkingManager extends ParkingBoy{
+    private ArrayList<ParkPlace> parkingBoy;
+
+    public ParkingManager(ArrayList<ParkPlace> parkPlaces, ParkingLotChooser parkingLotChooser) {
+        super(parkPlaces, parkingLotChooser);
+    }
 }
